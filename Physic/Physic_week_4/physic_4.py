76621f4814833c7e86420d3d5167e2ae2b85c563
Ken_Chang <ken205115331@gmail.com>
2022-03-28 22:05:28 +0800

    physic week 5 test

--- Physic/Physic_week_4/physic_4.py
+++ Physic/Physic_week_4/physic_4.py
@@ -7,11 +7,9 @@ from vpython import arange
 from vpython import rate
 import math
 
-# output_file = open("output.txt", "w")  # 提供檔案輸出方法（預設名稱為output.txt）
 
 # 小球的半徑（可隨意調整）
-R = 5
-
+R = 9
 # 畫出一顆不太小的球代表一群聚成球狀的電荷分布
 charge = sphere(radius=R, opacity=0.8)
 
@@ -19,13 +17,13 @@ charge = sphere(radius=R, opacity=0.8)
 sw = R/5
 
 # 畫一個箭頭代表 x 軸，並加上一個文字 X
-xaxis = arrow(axis=vector(1, 0, 0)*R*10, color=color.red, shaftwidth=sw)
+xaxis = arrow(axis=vector(1, 0, 0)*R*12, color=color.red, shaftwidth=sw)
 text(text="X", pos=xaxis.axis, color=xaxis.color, height=R*1.5)
 # 畫一個箭頭代表 y 軸，並加上一個文字 Y
-yaxis = arrow(axis=vector(0, 1, 0)*R*10, color=color.green, shaftwidth=sw)
+yaxis = arrow(axis=vector(0, 1, 0)*R*12, color=color.green, shaftwidth=sw)
 text(text="Y", pos=yaxis.axis, color=yaxis.color, height=R*1.5)
 # 畫一個箭頭代表 z 軸，並加上一個文字 Z
-zaxis = arrow(axis=vector(0, 0, 1)*R*10, color=color.cyan, shaftwidth=sw)
+zaxis = arrow(axis=vector(0, 0, 1)*R*12, color=color.cyan, shaftwidth=sw)
 text(text="Z", pos=zaxis.axis, color=zaxis.color, height=R*1.5)
 
 
@@ -37,11 +35,20 @@ charge.density = charge.q / (4/3)*math.pi*charge.radius**3
 epsilon = 8.854187817e-12
 
 
-for theta in arange(0, 180, 180/20):   # 天頂角從北極到南極，分成 20 步完成
+theta_step, phi_step = input("Please input the steps of theta \
+and phi.").split()
+inte_radius_step, inte_theta_step, inte_phi_step = input("Please \
+input the steps of theta and phi.").split()
+theta_step, phi_step = int(theta_step), int(phi_step)
+inte_radius_step, inte_theta_step, inte_phi_step = int(
+    inte_radius_step), int(inte_theta_step), int(inte_phi_step)
+
+
+for theta in arange(0, 180, 180/theta_step):   # 天頂角根據輸入步數，決定總步
 
     theta = theta*math.pi/180  # 換成弧度
 
-    for phi in arange(0, 360, 360/40):  # 方位角繞一整圈，分成 40 步完成
+    for phi in arange(0, 360, 360/phi_step):  # 方位角根據輸入步數，決定總步
 
         phi = phi*math.pi/180  # 換成弧度
 
@@ -53,11 +60,11 @@ for theta in arange(0, 180, 180/20):   # 天頂角從北極到南極，分成 20
 
         # 把這個球體由小積分到大，並用每個區塊的電荷計算該距離的電場
         # 將球的半徑分成許多小步計算
-        drp = charge.radius/5
+        drp = charge.radius/inte_radius_step
         # 將天頂角 thetap 分成許多小步計算
-        dthetap = math.pi/3
+        dthetap = math.pi/inte_theta_step
         # 將方位角 phip 分成許多小步計算
-        dphip = math.pi*2/6
+        dphip = math.pi*2/inte_phi_step
         E = vector(0, 0, 0)    # 電場先設定為 0，後面計算中會積分出來
 
         for rp in arange(0, charge.radius, drp):   # 積分半徑從 0 到 charge.radius
@@ -82,8 +89,17 @@ for theta in arange(0, 180, 180/20):   # 天頂角從北極到南極，分成 20
                     # 加總到 E
                     E += dE
 
+        # 每個箭頭的電場大小
+        print("mangitude of E:", E.mag, sep='')
         # 畫一個箭頭代表電場，乘上一個比例讓箭頭看起來大小合適
-        arrow(pos=r, axis=E*3e-15/8, shaftwidth=sw/3)
+        arrow(pos=r, axis=E*3e-15/10, shaftwidth=sw/3)
 
         # 以(1.0/frequency)的秒數定義一秒要執行幾個
         rate(1000)
+
+# 總共的積分步數
+print("Integration total step:", inte_phi_step
+      * inte_theta_step * inte_radius_step, sep='')
+# 全部總共的步數
+print("All total step:", theta_step * phi_step * inte_phi_step
+      * inte_theta_step * inte_radius_step, sep='')
